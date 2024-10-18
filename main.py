93dbac08e121c91cb4aecc385a280ea6138ebefb
Pavel Molev <onvon5404@gmail.com>
2024-10-18 12:13:44 +0300

    task2

--- main.py
+++ main.py
@@ -15,16 +15,37 @@
 
 
 # task 2
+# import random
+#
+# arrayLength = 10
+# array = []
+#
+# for _ in range(arrayLength):
+#     randomNumber = int((round(random.random(), 2)) * 200) - 100
+#     array.append(randomNumber)
+# print(array)
+
+# task 3
 import random
 
-arrayLength = 10
+arrayLength = int(input("Введите размер массива"))
 array = []
 
 for _ in range(arrayLength):
-    randomNumber = int((round(random.random(), 2)) * 200) - 100
+    randomNumber = random.randint(-50, 100)
     array.append(randomNumber)
-print(array)
 
+maxInt = array[0]
+minInt = array[0]
+
+for num in array:
+    if num > maxInt:
+        maxInt = num
+    if num < minInt:
+        minInt = num
+
+print(maxInt, minInt)
+# task 4
 
 
 
